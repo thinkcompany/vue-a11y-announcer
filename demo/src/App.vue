e2ff038d6b3719615d4ef37aad748dcab7edd77c
Luke Pettway <luke.pettway@thinkbrownstone.com>
2017-06-12 16:24:23 -0400

    Removed hello world example code

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,18 +1,12 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello></hello>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-
 export default {
-  name: 'app',
-  components: {
-    Hello
-  }
+  name: 'app'
 }
 </script>
 
