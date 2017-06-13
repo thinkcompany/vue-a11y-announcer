7d27701d629526769b5a7770ae2d68081813fe45
Luke Pettway <luke.pettway@thinkbrownstone.com>
2017-06-13 09:14:05 -0400

    Added the announce component to the demo for now, need to find a good way to copy it.

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,12 +1,33 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <announcer :text="announcement"></announcer>  
+    <button v-on:click="announce">Trigger a new announcement</button>
   </div>
 </template>
 
 <script>
+
+import Announcer from './announcer.vue'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    Announcer
+  },
+  methods: {
+    announce: function (event) {
+      this.announcement = 'Here\'s a new annoucement!'
+      setTimeout(() => {
+        this.announcement = ''
+      }, 2000)
+    }
+  },
+  data: function () {
+    return {
+      announcement: ''
+    }
+  }
 }
 </script>
 
